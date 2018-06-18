48fd6303e094cdf175cdac480678346771fc185e
Lukey <lshort2@mail.csuchico.edu>
2018-06-17 20:34:11 -0700

    checking everything in

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
  * Scoping hack to avoid multiple declaration error
  * when using back/forward buttons of browser
  */
-if(true) {
+if(false) {
   let apiHost = 'https://api.giphy.com';
   let endpoint = '/v1/gifs/random';
   let apiKeyQuery = '?api_key=yxOgJBtY9aYW8CTeJB5P2s8YhL3H4yvu';
@@ -12,23 +12,23 @@ if(true) {
   console.log('fullUrl:');
   console.log(fullUrl);
   
-//   fetch(fullUrl)
-//       .then(response => response.json())
-//       .then(data => {
-//           // Do what you want with your data
-//           console.log('I think we got some data.');
-//           console.log(data);
-//           let memeSrc = data.data.image_original_url;
-//           console.log('memeSrc');
-//           console.log(memeSrc);
-//           console.log('assigning it...?');
-//           $('#meme_target').attr('src', memeSrc);
-//           console.log('assignined?');
-//   
-//           setTimeout(function(){
-//           }, 0);
-//       })
-//       .catch(err => {
-//           console.error('We lost one!', err);
-//       });
+  fetch(fullUrl)
+      .then(response => response.json())
+      .then(data => {
+          // Do what you want with your data
+          console.log('I think we got some data.');
+          console.log(data);
+          let memeSrc = data.data.image_original_url;
+          console.log('memeSrc');
+          console.log(memeSrc);
+          console.log('assigning it...?');
+          $('#meme_target').attr('src', memeSrc);
+          console.log('assignined?');
+  
+          setTimeout(function(){
+          }, 0);
+      })
+      .catch(err => {
+          console.error('We lost one!', err);
+      });
 }
