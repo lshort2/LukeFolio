a64ed9dd63b8a162fb3940a81df41e5f8be7b23c
Lukey <lshort2@mail.csuchico.edu>
2018-06-18 19:33:46 -0700

    most of content appears to be done

--- app/view_github/github.js
+++ app/view_github/github.js
@@ -10,5 +10,8 @@ angular.module('myApp.github', ['ngRoute'])
 }])
 
 .controller('ViewGithubCtrl', [function() {
-  window.scrollTo(0, 0);
+  // window.scrollTo(0, 0);
+  $(".parallax").animate({
+    scrollTop: $(document).height()
+  }, 200);
 }]);
